4bb24dfa4408c5dfda9a2177944729c9d214993c
udhaya1494 <udhaya1494@gmail.com>
2022-08-05 11:24:23 +0530

    unwanted Commit

--- libs/shared-data-models/lib/signup/create_response.dart
+++ libs/shared-data-models/lib/signup/create_response.dart
@@ -3,11 +3,17 @@ abstract class CreateResponse {
   final String? message;
   final String? status;
   final String? userId;
+  final String? serviceFlow;
 
   CreateResponse({
     this.serviceRequestId,
     this.message,
     this.status,
     this.userId,
+    this.serviceFlow,
   });
+
+  // TODO: Should be using a freezed class here for the status
+  bool get isSucceeded =>
+      status?.toLowerCase() == 'succeeded' || status?.toLowerCase() == 'paused';
 }
