d419889c201732d0f4f02d4dcaf6df42b779b85f
udhaya1494 <udhaya1494@gmail.com>
2022-08-27 18:40:39 +0530

    Conflicts Changes and Firebase Crashlytics Implemented on Both Customer and Agent App

--- apps/customer/lib/main.dart
+++ apps/customer/lib/main.dart
@@ -1,13 +1,19 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:core/app/crayon_payment_material_app.dart';
 import 'package:core/ioc/di_container.dart';
+import 'package:core/logging/logger.dart';
 import 'package:core/navigation/navigation_manager.dart';
 import 'package:core/session_management/inactivity_service.dart';
 import 'package:core/session_management/inactivity_watcher.dart';
 import 'package:core/translation/crayon_payment_transaltions_loader.dart';
 import 'package:core/translation/crayon_payment_translations.dart';
 import 'package:device_option/view/device_option_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/src/framework.dart';
@@ -35,23 +41,52 @@ void main() async {
   );
   await translations.loadTranslationFiles();
   await CrayonPaymentTheme().initialize(loadCustomTheme: true);
-
   bool status = false;
 
   SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
   );
-  runApp(
-    ProviderScope(
-      key: Key('ProviderScope'),
-      child: HomeWidget(
-        translations,
-        DIContainer.container<NavigationManager>(),
-        DIContainer.container<IInactivityService>(),
-        status,
+
+  await Firebase.initializeApp();
+
+
+  if (!kDebugMode) {
+    runZonedGuarded(()  {
+       runApp(
+        ProviderScope(
+          key: Key('ProviderScope'),
+          child: HomeWidget(
+            translations,
+            DIContainer.container<NavigationManager>(),
+            DIContainer.container<IInactivityService>(),
+            status,
+          ),
+        ),
+      );
+    },(error, stack) {
+      if (FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        FirebaseCrashlytics.instance.recordError(error, stack);
+      }
+      CrayonPaymentLogger.logError('$error\n$stack');
+    },
+    );
+
+  } else {
+    runApp(
+      ProviderScope(
+        key: Key('ProviderScope'),
+        child: HomeWidget(
+          translations,
+          DIContainer.container<NavigationManager>(),
+          DIContainer.container<IInactivityService>(),
+          status,
+        ),
       ),
-    ),
-  );
+    );
+  }
+
+  await _setupCrashlytics();
+
 }
 
 class HomeWidget extends StatelessWidget {
@@ -85,6 +120,23 @@ class HomeWidget extends StatelessWidget {
           return child ?? Container();
         },
       ),
+    );
+  }
+}
+
+Future<void> _setupCrashlytics() async {
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    Isolate.current.addErrorListener(
+      RawReceivePort((pair) async {
+        final List<dynamic> errorAndStacktrace = pair;
+        await FirebaseCrashlytics.instance.recordError(
+          errorAndStacktrace.first,
+          errorAndStacktrace.last,
+        );
+      }).sendPort,
     );
   }
 }
