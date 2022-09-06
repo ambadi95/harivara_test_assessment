7af5572ae4fc63090867a21c0055b4616e68a303
Bharti <>
2022-09-06 17:13:17 +0530

    Worked on EN/SW Strings

--- libs/features/device_option/lib/sub_features/device_loan_creation/view/device_loan_creation_screen.dart
+++ libs/features/device_option/lib/sub_features/device_loan_creation/view/device_loan_creation_screen.dart
@@ -73,7 +73,12 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
   }
 
   Widget _createLoading() {
-    return  const CenteredCircularProgressBar(color: PRIMARY_COLOR);
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: const CenteredCircularProgressBar(color: PRIMARY_COLOR),
+      ),
+    );
   }
 
   void _listenToStateChanges(
@@ -87,10 +92,8 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
           const CrayonPaymentAppBarButtonType.back(),
         ],
       ),
-      bottomNavigationBar:
-      loanPreviewResponseModel != null
-          ?
-      Padding(
+      bottomNavigationBar: loanPreviewResponseModel != null
+          ? Padding(
               padding: const EdgeInsets.only(left: 16, right: 16, bottom: 18),
               child: selectButton(coordinator),
             )
@@ -155,7 +158,14 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
           Divider(),
           dynamicHSpacer(10),
           loanPreviewResponseModel == null
-              ? SizedBox()
+              ? CrayonPaymentText(
+                  key: Key('${_identifier}_ERROR_MESSAGE'),
+                  text: TextUIDataModel(
+                      'ERROR_MESSAGE'.tr,
+                      styleVariant: CrayonPaymentTextStyleVariant.headline4,
+                      color: AN_TitleColor,
+                      fontWeight: FontWeight.w600),
+                )
               : _loanDetails(context),
           dynamicHSpacer(20),
         ],
@@ -185,27 +195,26 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
 
   Widget selectButton(DeviceLoanCreationCoordinator coordinator) {
     return CrayonPaymentDockedButton(
-      key:  Key('Select'),
-      title: "${'DLC_pay_now'.tr} ${loanPreviewResponseModel!.data!.totalAmountToBeRepaid} TZSHS",
-      // title: 'Pay Now 2000 TZHS',
+      key: const Key('Select'),
+      title:
+          'Pay Now ${loanPreviewResponseModel!.data!.totalAmountToBeRepaid}TZHS',
       borderRadius: 8,
       height: CrayonPaymentDimensions.marginFortyEight,
       buttonColor: LS_ButtonColor,
       textColor: White,
       textStyleVariant: CrayonPaymentTextStyleVariant.headline4,
       onPressed: () {
-        coordinator.navigateToDownPayment(loanPreviewResponseModel!.data!.totalAmountToBeRepaid!.toString(),detailDetail!.deviceId);
+        coordinator.navigateToDownPayment(detailDetail?.deviceId);
       },
     );
   }
 
   _loanDetails(context) {
-    print(loanPreviewResponseModel!.data!.toJson());
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CrayonPaymentText(
-          key: Key('loan details'),
+          key: Key(''),
           text: TextUIDataModel('DLC_Loan_Details'.tr,
               styleVariant: CrayonPaymentTextStyleVariant.headline4,
               color: AN_CardTitle,
@@ -226,7 +235,7 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
             loanPreviewResponseModel!.data!.finalPaymentDate.toString()),
         dynamicHSpacer(40),
         CrayonPaymentText(
-          key: Key('paymnet date'),
+          key: Key(''),
           text: TextUIDataModel('DLC_Wallets_Title'.tr,
               styleVariant: CrayonPaymentTextStyleVariant.headline4,
               color: AN_CardTitle,
@@ -248,7 +257,7 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
             Expanded(
               flex: 1,
               child: CrayonPaymentText(
-                key: Key('key value'),
+                key: Key(''),
                 text: TextUIDataModel(key,
                     styleVariant: CrayonPaymentTextStyleVariant.headline5,
                     color: AN_CardTitle,
@@ -258,7 +267,7 @@ class _DeviceLoanCreationScreenState extends State<DeviceLoanCreationScreen> {
             Expanded(
               flex: 1,
               child: CrayonPaymentText(
-                key: Key('data value'),
+                key: Key(''),
                 text: TextUIDataModel(value,
                     styleVariant: CrayonPaymentTextStyleVariant.headline5,
                     color: AN_CardTitle,
