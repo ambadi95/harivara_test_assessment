ee24f35cd0b5b1836c03356918defbc4a5a06dca
udhaya1494 <udhaya1494@gmail.com>
2022-09-07 16:50:16 +0530

    Passcode g Part file

---
+++ libs/shared-data-models/lib/check_passcode_response/data.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Data _$DataFromJson(Map<String, dynamic> json) => Data(
+      passcodeSet: json['passcodeSet'] as bool?,
+    );
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'passcodeSet': instance.passcodeSet,
+    };
