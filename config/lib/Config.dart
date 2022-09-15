c03cd8d8f623dc3d33f820c982c004357049b6bc
udhaya1494 <udhaya1494@gmail.com>
2022-09-15 21:32:19 +0530

    Endpoint Changed

--- config/lib/Config.dart
+++ config/lib/Config.dart
@@ -37,10 +37,10 @@ const ST_agent = 'assets/images/agent.svg';
 const ST_scan_icon = 'assets/images/scan_image.svg';
 
 //live Link
-// const customerEndpoint = 'https://customerapi.y9bank.com/customers/v1/';
+const customerEndpoint = 'https://customerapi.y9bank.com/customers/v1/';
 //pre prod
 // const customerEndpoint = 'https://customerapi-stage.y9bank.com/customers/v1/';
-const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
+// const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
 //dev
 // const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
 
