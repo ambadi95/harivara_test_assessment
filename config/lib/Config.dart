38e8aa3e832528b980675c24dbca8c520fb65df6
Parvinder <parvinder@crayondata.com>
2022-09-24 15:21:14 +0530

    latest code

--- config/lib/Config.dart
+++ config/lib/Config.dart
@@ -41,10 +41,10 @@ const ST_scan_icon = 'assets/images/scan_image.svg';
 //live Link
 // const customerEndpoint = 'https://customerapi.y9bank.com/customers/v1/';
 //pre prod
-//const customerEndpoint = 'https://customerapi-stage.y9bank.com/customers/v1/';
+const customerEndpoint = 'https://customerapi-stage.y9bank.com/customers/v1/';
 // const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
 //dev
- const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
+//  const customerEndpoint = 'https://y9-dev-capi.testmaya.com/customers/v1/';
 
 const y9TermsCondition = 'https://y9bank.com/term-of-services/';
 
